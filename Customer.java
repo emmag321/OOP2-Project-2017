a932ef0d8f7f92ff8a344e4f907c47771c3274df
Emma <emmag321@gmail.com>
2017-11-08 12:25:44 +0000

    Start of project 07/11/207 @ 23.33PM

--- Customer.java
+++ Customer.java
@@ -1,4 +1,52 @@
 package OOP2_Project_MyShop;
 
 public class Customer {
+
+    //attributes
+    private String firstName;
+    private String lastName;
+    private String address;
+    private int phoneNum;
+
+    //mutators
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public void setPhoneNum(int phoneNum)
+    {
+        this.phoneNum = phoneNum;
+    }
+
+    //accessors
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public int getPhoneNum()
+    {
+        return phoneNum;
+    }
 }
