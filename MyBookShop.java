a0a4f528d29e6bd8e961eec5601a467f72310ad9
Emma <emmag321@gmail.com>
2017-11-13 22:36:06 +0000

    Progress made 13/11/207 @ 22.35PM

--- MyBookShop.java
+++ MyBookShop.java
@@ -8,28 +8,35 @@ import java.io.*;
 
 
 
-public class MyBookShop extends JFrame /*implements ActionListener*/{
+public class MyBookShop extends JFrame /*implements ActionListener*/ {
 
-    //MyBookShop cust1;
+    JMenu fileMenu;
+    MyBookShop book1;
 
     //array
     Customer cust1 = new Customer();
     ArrayList<Customer> customers;
 
     //customer
-    String firstName, lastName, address;
-    float phoneNum;
+    /*String firstName, lastName, address;
+    float phoneNum;*/
+
+    //book
+    String title, author,isbn;
+    int numPages;
+    double price;
+
 
     public static void main(String[] args) {
         MyBookShop book1 = new MyBookShop();
         book1.setVisible(true);
     }
 
-    public MyBookShop(){
+    public MyBookShop() {
         super("Book");
 
-        setSize(500,500);
-        setLocation(200,200);
+        setSize(500, 500);
+        setLocation(200, 200);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
         //addWindowListener(new WindowEventHandler());
@@ -37,6 +44,20 @@ public class MyBookShop extends JFrame /*implements ActionListener*/{
         Container cPane = getContentPane();
         cPane.setLayout(new FlowLayout());
         cPane.setBackground(Color.DARK_GRAY);
+
+        //menu
+        createFileMenu();
+        createCustomersMenu();
+        createOptionsMenu();
+    }
+
+    private void createFileMenu() {
+    }
+
+    private void createCustomersMenu() {
+    }
+
+    private void createOptionsMenu() {
     }
 
 }
