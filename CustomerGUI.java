e6606276ff37be1a9d8f4a30212074f3239abc15
t00185959 <emmag321@gmail.com>
2017-11-30 15:11:38 +0000

    30/11/2017 @ 15.11PM

--- CustomerGUI.java
+++ CustomerGUI.java
@@ -4,20 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 public class CustomerGUI {
 
     private static JFrame mainframe=new JFrame("Customer");
     ArrayList<Book> books = AdminGUI.books;
+    ArrayList<JButton> buttons;
     ArrayList<Person> employees = AdminGUI.employees;
     private static Book actualBook=null;
 
+    //declaring JLabel here
+    JLabel text;
+
+    //this is where my process is taking place - the purchasing of a book
     public CustomerGUI(){
+        //the frame will close when 'x' is pressed
         mainframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //default settings of the frame
         mainframe.setBounds(200,200,500,500);
-        JPanel flowy=new JPanel(new FlowLayout());
+        //creating new JPanel here
+        JPanel flowLayout=new JPanel(new FlowLayout());
         GridLayout grid=null;
+
+        //this is my process here
         if(books.size()==0||books.size()==1){
             grid=new GridLayout(1,1);
         }else if (books.size()==2){
@@ -29,28 +42,67 @@ public class CustomerGUI {
         }
         JPanel contentPanel=new JPanel(grid);
 
+        buttons = new ArrayList<JButton>();
+
         for (int i=0;i<books.size();i++){
-            actualBook=null;
+
             actualBook=books.get(i);
             JButton btn=new JButton(actualBook.getTitle());
+            buttons.add(btn);
+            System.out.println(buttons.get(i));
+
             btn.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
+
+                    for(int i=0;i<buttons.size();i++)
+                    {
+                        if(buttons.get(i).getText().equals(btn.getText()))
+                                actualBook = books.get(i);
+                    }
+
+
+                    //if(buttons.contains(btn.getText()))
+                     //   System.out.println(buttons.get())
+                    //System.out.println(btn.getText());
+
                     actualBook.setNumMinusOne();
+                    System.out.println(actualBook);
                     JOptionPane.showMessageDialog(null,"Thank you for purchasing the book:"+actualBook.getTitle());
+
+
+
+                    if (books.size() > 0) {
+                        try {
+                            FileOutputStream fileOut = new FileOutputStream("books.dat");
+                            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                            out.writeObject(books);
+                            out.close();
+                            fileOut.close();
+                            System.out.printf("Serialized data to books.dat file");
+                        } catch (IOException i) {
+                            JOptionPane.showMessageDialog(
+                                    null,
+                                    "Failed to save books",
+                                    "Error",
+                                    JOptionPane.ERROR_MESSAGE
+                            );
+                            i.printStackTrace();
+                        }
+                    }
                 }
             });
             contentPanel.add(btn);
         }
         contentPanel.setVisible(true);
-        flowy.add(contentPanel);
+        flowLayout.add(contentPanel);
 
 
-
-        JTextField text=new JTextField("By Clicking on the buttons you'll purchase the item");
+        JLabel text = new JLabel("By Clicking on the buttons you'll purchase the item");
+        //JTextField text=new JTextField("By Clicking on the buttons you'll purchase the item");
         text.setVisible(true);
-        flowy.add(text);
-        mainframe.add(flowy);
+        flowLayout.add(text);
+        mainframe.add(flowLayout);
         mainframe.setVisible(true);
     }
 }
